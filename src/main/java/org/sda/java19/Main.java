e4ffeaaf288a94c2cee38fbb13e1caa061eba8ac
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-11-21 19:08:25 +0200

    Added exercise

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,5 +1,19 @@
 package org.sda.java19;
 
+/**
+ * Exercise-1
+ * Create 3 classes: Person, Developer, JavaDeveloper.
+ * a. Person is a parent class, Developer inherits from Person, JavaDeveloper inherits from
+ * Developer
+ * b. Create constructor for every class that will call constructor of the super class. Each
+ * constructor should display an information, that it has been called.
+ * c. Create an object of type JavaDeveloper. What information will be displayed and in
+ * what order?
+ * d. Using an object of type JavaDeveloper call a method that is defined in Developer
+ * class. What access modifier should it have?
+ * e. *Overload method from the Person class in JavaDeveloper class to accept additional
+ * parameters.
+ */
 public class Main {
     public static void main(String[] args) {
 
