70dae03d78a8b4cfb0171b36ba6b3daf00470ac0
Vinod John <vinodjohn56@gmail.com>
2022-11-24 20:46:04 +0200

    Exercise added

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,5 +1,12 @@
 package org.sda.java19;
 
+/**
+ * Personal information
+ * a. Create a file containing any personal data (name, surname, phone number). Data of individual persons should be in the following lines.
+ * b. Download data from a file and create objects of people based on them (in any way - Regex, String.split ...).
+ * c. Enter the created objects into ArrayList or Map (<line number>: <Person>).
+ * d. Present the obtained data.
+ */
 public class Main {
     public static void main(String[] args) {
 
